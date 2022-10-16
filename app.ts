1c082bbdd0dcde0481070e7aa3cdca0296821460
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-10-16 10:18:56 +0300

    ignore node_modules and adding env variable

--- app.ts
+++ app.ts
@@ -1,8 +1,9 @@
 
+require("dotenv").config();
 require("./model/db")
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.DEV_PORT;
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 const Routes = require("./routes/")
--- model/db.ts
+++ model/db.ts
@@ -1,6 +1,4 @@
 
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://Clement:Cle*1995@cluster0-my6sq.mongodb.net/Grouped_products?retryWrites=true&w=majority"
-mongoose.connect(uri);
-
+mongoose.connect(process.env.mongoURI);
 require('./schema');
